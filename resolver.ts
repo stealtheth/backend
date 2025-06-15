5101871e34ffb12392b8760978d44dec120d7d58
zkfriendly <zkfriendly@proton.me>
2025-06-15 10:45:45 +0200

    feat: resolve all ens queries with a random address each time

--- resolver.ts
+++ resolver.ts
@@ -1,24 +1,43 @@
-import express, { type Request, type Response, type NextFunction, type Express } from 'express';
-const app: Express = express();
-
-app.use(express.json());
-
 const port = process.env.PORT || 8080;
 
-app.get(
-  '/',
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      res.status(200).json({
-        message: 'Hurray!! we create our first server on bun js',
-        success: true,
-      });
-    } catch (error: unknown) {
-      next(new Error((error as Error).message));
-    }
-  },
-);
+Bun.serve({
+  port,
+  async fetch(req) {
+    const url = new URL(req.url);
 
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
+    if (req.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      });
+    }
+
+    if (url.pathname === '/' && req.method === 'POST') {
+      const body = await req.json();
+      console.log({ req: body })
+
+      const address = `0x${Array.from({ length: 40 }, () => Math.floor(Math.random() * 16).toString(16)).join('')}`;
+      const encodedAddress = `0x${'0'.repeat(24)}${address.substring(2)}`;
+      const ccipReadRes = { data: encodedAddress };
+
+      return new Response(JSON.stringify(ccipReadRes), {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    return new Response('Not Found', { status: 404 });
+  },
+  error(error) {
+    return new Response(`<pre>${error}\n${error.stack}</pre>`, {
+      headers: { 'Content-Type': 'text/html' },
+    });
+  },
 });
+
+console.log(`Server is up and running on port ${port}`);
